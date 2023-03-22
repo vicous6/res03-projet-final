50384156ca9dd328230aeca012a7020b5ff454d6
vicous6 <vicous@free.fr>
2023-03-22 16:30:35 +0000

    le prix fonctionne avec les decimals

--- assets/js/cart/makeTotalPrice.js
+++ assets/js/cart/makeTotalPrice.js
@@ -3,14 +3,33 @@ function total (){
     let prices = document.getElementsByClassName("prix");
     let displayTotal = document.getElementById("cart-total-price")
     console.log(prices);
-    total = 0;
+    let total = 0.0;
     for(let i = 0 ; i < prices.length ; i++){
         
-        console.log(prices[i].innerHTML)
-        total += parseInt( prices[i].innerHTML)
+     
+        let length = prices[i].innerHTML.length
+        let StringOfNumber = []
+        console.log(length)
+        for(let j = 0 ; j < length; j ++){
+            
+            // console.log(prices[i].innerHTML[j])
+            if(prices[i].innerHTML[j] !== " "){
+                
+                StringOfNumber+=prices[i].innerHTML[j]
+                console.log(StringOfNumber)
+            }else{
+                let value = parseFloat(StringOfNumber)
+                total += value;
+                console.log(value, total)
+                break
+            }
+        }
+        
+        
+        // total += parseInt( prices[i].innerHTML)
     }
-    let total2 = total.toString()
-    displayTotal.innerHTML= "Total = "+total2+" euros"
+    
+    displayTotal.innerHTML= "Total = "+total.toFixed(2)+" euros"
 }
 
 export { total };
