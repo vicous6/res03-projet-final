46b414a7900d68484e9821a154709a9c665ccc2f
vicous6 <vicous@free.fr>
2023-05-03 13:16:33 +0000

    commit final

--- assets/js/initHeader.js
+++ assets/js/initHeader.js
@@ -9,9 +9,9 @@ function initHeader(){
         all[1].classList.add("animation2")
         all[2].classList.add("animation1")
         all[3].classList.add("animation3")
-        all[4].classList.add("animation3")
+        all[4].classList.add("animation1")
         all[5].classList.add("animation2")
-        all[6].classList.add("animation2")
+        all[6].classList.add("animation3")
         all[7].classList.add("animation4")
         
     
